57503f3c80e04fcdce963aa0b7c003973839487b
Roman Shtylman <shtylman@gmail.com>
2013-12-30 13:16:41 -0500

    preseed some globals we know we will leak on older IE

--- test/index.js
+++ test/index.js
@@ -1,5 +1,24 @@
 var env = require('./support/env');
 
+// the jsonp polling stuff sets this global
+// in order to avoid leak detection we prefill here
+if (global.navigator && navigator.appName === 'Microsoft Internet Explorer') {
+  if (navigator.appVersion.indexOf('MSIE 6') >= 0 ||
+      navigator.appVersion.indexOf('MSIE 7') >= 0 ||
+      navigator.appVersion.indexOf('MSIE 8') >= 0 ||
+      navigator.appVersion.indexOf('MSIE 9') >= 0
+      ) {
+    global.___eio = undefined;
+  }
+
+  if (navigator.appVersion.indexOf('MSIE 9') >= 0) {
+    global.WEB_SOCKET_LOGGER = undefined;
+    global.WEB_SOCKET_SUPPRESS_CROSS_DOMAIN_SWF_ERROR = undefined;
+    global.WEB_SOCKET_DISABLE_AUTO_INITIALIZATION = undefined;
+    global.WEB_SOCKET_SWF_LOCATION = undefined;
+  }
+}
+
 require('./engine.io-client');
 require('./util');
 require('./parser');
