6db6444c3a96a7078ece58ff7e05017df5ccfec6
SQReder <sqreder@gmail.com>
2019-08-17 14:24:42 +0300

    build: Migrate to Gulp 4
    BREAKING CHANGES: Use `gulp --tasks` instead of `gulp help`

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,8 @@ const mocha = require('gulp-mocha');
 const istanbul = require('gulp-istanbul');
 const webpack = require('webpack-stream');
 const child = require('child_process');
-const help = require('gulp-task-listing');
 const del = require('del');
 const eslint = require('gulp-eslint');
-
-gulp.task('help', help);
 
 // //////////////////////////////////////
 // BUILDING
@@ -15,13 +12,15 @@ gulp.task('help', help);
 
 const BUILD_TARGET_DIR = './';
 
-gulp.task('default', ['build']);
-
-gulp.task('build', function () {
+function build() {
   return gulp.src('lib/**/*.js')
     .pipe(webpack(require('./support/webpack.config.js')))
     .pipe(gulp.dest(BUILD_TARGET_DIR));
-});
+}
+
+exports.default = build;
+
+exports.build = build;
 
 // //////////////////////////////////////
 // TESTING
@@ -37,15 +36,21 @@ const FILES_TO_CLEAN = [
   'test/support/public/engine.io.js'
 ];
 
-gulp.task('test', ['lint'], function () {
-  if (process.env.hasOwnProperty('BROWSERS')) {
-    return testZuul();
-  } else {
-    return testNode();
-  }
-});
+// gulp.task('test', ['lint'], test);
 
-gulp.task('lint', function () {
+function test(done) {
+  if (process.env.hasOwnProperty('BROWSERS')) {
+    return testZuul(done);
+  } else {
+    return testNode(done);
+  }
+}
+
+exports.test = gulp.series(lint, test);
+
+// gulp.task('lint', lint);
+
+function lint() {
   return gulp.src([
     '*.js',
     'lib/**/*.js',
@@ -55,59 +60,75 @@ gulp.task('lint', function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+}
 
-gulp.task('test-node', testNode);
-gulp.task('test-zuul', testZuul);
+exports.lint = lint;
 
-function testNode () {
-  return gulp.src(TEST_FILE, { read: false })
+// gulp.task('test-node', testNode);
+exports['test-node'] = gulp.series(testNode);
+// gulp.task('test-zuul', testZuul);
+exports['test-zuul'] = gulp.series(testZuul);
+
+function testNode(done) {
+  return gulp.src(TEST_FILE, {read: false})
     .pipe(mocha(MOCHA_OPTS))
     // following lines to fix gulp-mocha not terminating (see gulp-mocha webpage)
     .once('error', function (err) {
       console.error(err.stack);
       cleanFiles(FILES_TO_CLEAN);
+      done();
       process.exit(1);
     })
     .once('end', function () {
       cleanFiles(FILES_TO_CLEAN);
+      done();
       process.exit();
     });
 }
 
 // runs zuul through shell process
-function testZuul () {
+function testZuul(done) {
   const ZUUL_CMD = './node_modules/zuul/bin/zuul';
-  const zuulChild = child.spawn(ZUUL_CMD, [TEST_FILE], { stdio: 'inherit' });
+  const zuulChild = child.spawn(ZUUL_CMD, [TEST_FILE], {stdio: 'inherit'});
   zuulChild.on('exit', function (code) {
     cleanFiles(FILES_TO_CLEAN);
-    process.exit(code);
+    if (code === 0) {
+      done();
+    } else {
+      done.fail(code);
+    }
   });
 }
 
-function cleanFiles (globArray) {
+function cleanFiles(globArray) {
   return del.sync(globArray);
 }
 
-gulp.task('istanbul-pre-test', function () {
+// gulp.task('istanbul-pre-test', istanbulPreTest);
+
+function istanbulPreTest() {
   return gulp.src(['lib/**/*.js'])
-    // Covering files
+  // Covering files
     .pipe(istanbul())
     // Force `require` to return covered files
     .pipe(istanbul.hookRequire());
-});
+}
 
-gulp.task('test-cov', ['istanbul-pre-test'], function () {
+// gulp.task('test-cov', ['istanbul-pre-test'], testCov);
+
+function testCov(done) {
   return gulp.src(TEST_FILE)
     .pipe(mocha(MOCHA_OPTS))
     .pipe(istanbul.writeReports())
     .once('error', function (err) {
       cleanFiles(FILES_TO_CLEAN);
       console.error(err.stack);
-      process.exit(1);
+      done.fail();
     })
     .once('end', function () {
       cleanFiles(FILES_TO_CLEAN);
-      process.exit();
+      done();
     });
-});
+}
+
+exports['test-cov'] = gulp.series(istanbulPreTest, testCov);
